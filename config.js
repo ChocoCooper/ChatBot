3b9281b5b1f0cca6ee3a1dcdd9c192f6686a1e9e
Harish K <kpharish2001@gmail.com>
2026-03-01 21:04:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // config.js
 const CONFIG = {
     API_KEY: "AIzaSyCExB2UpUqbupaIx8f8tdkxr1DTtOaxAiA",
-    YOUTUBE_API_KEY: "AIzaSyB8optqezh3fNj_c8jiQRWAdgX1qsPjFwI",
-    SUPABASE_URL: "https://crkgzxsjseparkkiubow.supabase.co",
-    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNya2d6eHNqc2VwYXJra2l1Ym93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNzA0NzgsImV4cCI6MjA4Nzk0NjQ3OH0.F98aNcm8988oBOdYVFvJgUY4IDW9Fe0pp3ITcBoibto"
+    YOUTUBE_API_KEY: "AIzaSyB8optqezh3fNj_c8jiQRWAdgX1qsPjFwI"
 };
