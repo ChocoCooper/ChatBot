e557778e9e949a0a3b717566647357bc1f852bbd
Harish K <kpharish2001@gmail.com>
2026-03-16 20:28:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 const CONFIG = {
     API_KEY: "AIzaSyCExB2UpUqbupaIx8f8tdkxr1DTtOaxAiA",
     YOUTUBE_API_KEY: "AIzaSyB8optqezh3fNj_c8jiQRWAdgX1qsPjFwI"
+    NEWS_API_KEY = '160dc0cbaa4d6ea4025f5f7d0fe6eed5'; 
 };
