d8d48d005d1b5ac18d845d1539cfbf281590d6ae
Harish K <kpharish2001@gmail.com>
2026-03-16 21:10:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    API_KEY: "",
-    YOUTUBE_API_KEY: "",
-    NEWS_API_KEY: ""
+    API_KEY: process.env.API_KEY,
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    NEWS_API_KEY: process.env.NEWS_API_KEY
 };
